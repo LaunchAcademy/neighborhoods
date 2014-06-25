30b99b0e70781a4653de35efdc9779fc7a078e1a
Dan Kleiman <info@dankleiman.com>
2014-06-25 17:45:47 -0400

    forgery, forget it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +8,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :avatar
   end
-  protect_from_forgery with: :exception
 end
