cf1c558cfc24aade8010396091730dd6121f4541
Omid Omid <omidbachari@gmail.com>
2014-06-25 15:42:12 -0400

    refactored image test

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,7 +17,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   def default_url
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("/fallback/" + [version_name, "default.png"].compact.join('_'))
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- spec/features/avatar_functionality_spec.rb
+++ spec/features/avatar_functionality_spec.rb
@@ -9,14 +9,18 @@ feature 'users can add an avatar', %Q(
 
   # Acceptance Criteria
 
-  # When I visit the edit profile page, I am presented with a file upload field, browse button, and upload button.
-  # When I select a proper file and click on upload, I get a success message and see the image on the page.
-  # When I select the wrong file type, I get an error message and can browse for another file.
+  # When I visit the edit profile page, I am presented with a
+  # file upload field, browse button, and upload button.
+  # When I select a proper file and click on upload, I get a
+  # success message and see the image on the page.
+  # When I select the wrong file type, I get an error message
+  # and can browse for another file.
 
   scenario 'user sees the proper field and browse and upload buttons' do
     attrs = {
       email: "dog@face.com",
       password: "Secret12345",
+
     }
 
     user = User.create(attrs)
@@ -26,14 +30,13 @@ feature 'users can add an avatar', %Q(
     click_on 'Edit Profile'
 
     expect(page).to have_content 'Add an avatar'
-    attach_file('user_avatar','spec/fixtures/avatar.jpg')
+    attach_file('user_avatar', 'spec/fixtures/avatar.jpg')
     fill_in 'Current password', with: user.password
     click_button 'Update'
+
+    user.reload
+
     expect(page).to have_content 'You updated your account successfully.'
-    expect(page).to have_css('img', text: user.avatar.url)
-
+    expect(page).to have_image user.avatar.url
   end
-
-
-
 end
