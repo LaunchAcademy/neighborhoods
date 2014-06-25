4402037fceda210321cd0ef816452b44199a0d55
Omid Omid <omidbachari@gmail.com>
2014-06-25 13:12:47 -0400

    Saved unsaved file

--- spec/features/avatar_functionality_spec.rb
+++ spec/features/avatar_functionality_spec.rb
@@ -31,6 +31,7 @@ feature 'users can add an avatar', %Q(
     click_button 'Update'
     expect(page).to have_content 'You updated your account successfully.'
     expect(page).to have_css('img', text: user.avatar.url)
+
   end
 
 
