5a91b4794befc82026ea7966437281f5ee251753
Dan Kleiman <info@dankleiman.com>
2014-06-27 09:50:38 -0400

    Indentation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ActiveRecord::Base
   validates :description, presence: true
   validates :rating, numericality: {
-      greater_than_or_equal_to: 1,
-      less_than_or_equal_to: 10
-    }
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 10
+  }
 
   belongs_to :user
   belongs_to :neighborhood
