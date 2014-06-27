f9526d1dcf094e1393fe5f308a6982e41aa3e6f9
Dan Kleiman <info@dankleiman.com>
2014-06-27 11:33:32 -0400

    Refactored reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,21 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @neighborhood = Neighborhood.find(params[:neighborhood_id])
     @review = Review.new
   end
 
   def create
-    @review = Review.new(review_params)
-    @review.neighborhood_id = params[:neighborhood_id]
-    @review.user_id = current_user.id
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @review = @neighborhood.reviews.build(review_params)
+    @review.user = current_user
     if @review.save
       flash[:notice] = 'Successfully added.'
       redirect_to neighborhood_path(@review.neighborhood)
     else
       flash[:alert] = 'Could not save.'
-      redirect_to neighborhood_path(params[:neighborhood_id])
+      render :new
     end
   end
 
