8e444b37fb190fbb7d8663f2d2ca6d0bef6fae32
Omid Omid <omidbachari@gmail.com>
2014-06-27 17:00:54 -0400

    Erased extra spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'neighborhoods#index'
 
-
   resources :neighborhoods, only: [:index, :new, :create, :show, :update] do
     resources :reviews, only: [:new, :create]
   end
