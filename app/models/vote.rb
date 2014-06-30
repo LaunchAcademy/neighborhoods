8911b0a51f069b780468d1e63b9e5af57d02fb61
Eric Kelly <heroiceric@gmail.com>
2014-06-30 16:56:59 -0400

    Refactor neighborhood show -page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,12 @@ class Vote < ActiveRecord::Base
   validates :user, presence: true
   validates :review, presence: true
   validates :weight, inclusion: { in: [1, -1] }
+
+  def upvote?
+    weight == 1
+  end
+
+  def downvote?
+    weight == -1
+  end
 end
