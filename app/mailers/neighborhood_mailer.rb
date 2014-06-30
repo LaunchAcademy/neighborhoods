718ee124f54c044291b3baab0c3b7f1c737523c6
Alek <agrinb@gmail.com>
2014-06-30 14:40:49 -0400

    updated app/mailers/neighborhood_mailer.rb

--- app/mailers/neighborhood_mailer.rb
+++ app/mailers/neighborhood_mailer.rb
@@ -1,28 +1,28 @@
 class NeighborhoodMailer < ActionMailer::Base
   default from: "alek@example.com"
 
-  #email to admin to approve neighborhood
+  # email to admin to approve neighborhood
   def new_neighborhood_email(neighborhood)
     @neighborhood = neighborhood
     @admin = User.find_by email: "agrinb@gmail.com"
     mail(to: @admin.email, subject: "New Neighborhood Submitted")
   end
 
-  #emails to neighborhood poster
+  # emails to neighborhood poster
   def neighborhood_approved_email(neighborhood)
     @neighborhood = neighborhood
     @user = @neighborhood.user
     mail(to: @user.email, subject: "@neighborhood.name Aprroved")
   end
 
-  #email when a neighborhood is declined
+  # email when a neighborhood is declined
   def neighorhood_declined_email(neighborhood)
     @neighborhood = neighborhood
     @user = @neighborhood.user
     mail(to: @user.email, subject: "@neighborhood.name Declined")
   end
 
-  #email when a review is create
+  # email when a review is create
   def neighborhood_review_email(neighborhood)
     @neighborhood = neighborhood
     @user = @neighborhood.user
