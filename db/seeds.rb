d6973d995cba6de7935c9c011ae0233e911996a5
Dan Kleiman <info@dankleiman.com>
2014-07-02 13:43:23 -0400

    Generated seed file with reviews and votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+user = FactoryGirl.create(:user, email: 'admin@neighborhoods.com', role: 'admin')
+
+users = 50.times.map do
+  FactoryGirl.create(:user, email: Forgery(:internet).email_address, password: 'neighborhood3')
+end
+
+neighborhoods = []
+CSV.foreach("lib/data/neighborhood_data.csv", headers: true) do |row|
+  neighborhoods << FactoryGirl.create(:neighborhood, name: row['name'], description: row['description'], user: users.sample, approved: true)
+end
+
+reviews = []
+
+neighborhoods.each do |neighborhood|
+  CSV.foreach("lib/data/review_data.csv", headers: true) do |row|
+    reviews << FactoryGirl.create(:review, description: row['description'],
+      user: users.sample, neighborhood: neighborhood, rating: rand(1..10))
+  end
+end
+
+weight = [1, -1]
+
+1000.times do
+  Vote.create(user: users.sample, review: reviews.sample, weight: weight.sample)
+end
